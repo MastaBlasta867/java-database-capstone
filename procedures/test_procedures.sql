e9a9e798dc7e6871a8d5b0800e3443b8877b560a
MastaBlasta867 <pomolefe1986@gmail.com>
2026-01-09 17:48:08 +0000

    Added test script for stored procedures

---
+++ procedures/test_procedures.sql
@@ -0,0 +1,8 @@
+-- Test Daily Appointment Report
+CALL GetDailyAppointmentReportByDoctor('2025-04-15');
+
+-- Test Doctor with Most Patients by Month
+CALL GetDoctorWithMostPatientsByMonth(4, 2025);
+
+-- Test Doctor with Most Patients by Year
+CALL GetDoctorWithMostPatientsByYear(2025);
