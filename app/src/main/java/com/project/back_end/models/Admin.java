960110aa5266b3d0a94b59af5449c1de60fce2ea
MastaBlasta867 <pomolefe1986@gmail.com>
2026-01-13 17:55:23 -0500

    Containerized backend, added Admin email field, fixed DB config

--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -17,23 +17,50 @@ public class Admin {
     @NotNull(message = "username cannot be null")
     private String username;
 
+    @NotNull(message = "email cannot be null")
+    private String email;
+
     @NotNull(message = "password cannot be null")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     public Admin() {}
 
-    public Admin(String username, String password) {
+    public Admin(String username, String password, String email) {
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() { 
+        return id; 
+    }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public void setId(Long id) { 
+        this.id = id; 
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getUsername() { 
+        return username; 
+    }
+
+    public void setUsername(String username) { 
+        this.username = username; 
+    }
+
+    public String getEmail() { 
+        return email; 
+    }
+
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+
+    public String getPassword() { 
+        return password; 
+    }
+
+    public void setPassword(String password) { 
+        this.password = password; 
+    }
 }
